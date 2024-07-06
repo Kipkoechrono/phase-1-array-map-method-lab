5c37c788c08be8c14c1d968e58b4e10930b3fe1a
peter <peter.rono@student.moringaschool.com>
2024-07-06 11:52:21 +0300

    added the titleCased function which processes the tutorials array

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    // Split each tutorial title into an array of words
+    return tutorial.split(' ').map(word => {
+      //Capitalize the first letter for each word and contactnates with the rest of the string
+      return word.charAt(0).toUpperCase() + word.slice(1);
+      // Join the array of with the first words uppercased to a single string.
+    }).join(' ');
+  });
 }
+// call the function to test it
+console.log(titleCased());
+ 
